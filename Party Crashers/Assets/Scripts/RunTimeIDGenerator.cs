a3f76691b4f9e7cc95be1635339e256fc6132967
dg015 <bruno.barbosa8@yahoo.com>
2026-08-02 22:15:30 -0300

    Created an runtime ID generator (MAKE IT INTO LIBRARY LATER)

--- Party Crashers/Assets/Scripts/RunTimeIDGenerator.cs
+++ Party Crashers/Assets/Scripts/RunTimeIDGenerator.cs
@@ -4,45 +4,58 @@ using UnityEngine;
 /// <summary>
 /// This script is a tool to generate runtimeIDs for scripts and objects;
 /// </summary>
-public class RunTimeIDGenerator : MonoBehaviour
+public class RunTimeIDGenerator 
 {
     [SerializeField] private int m_IDLenght;
     [SerializeField] private int m_maxNumber;
 
-    [SerializeField] private Dictionary<string,int> m_IDs = new Dictionary<string, int>();
-
-    [SerializeField] private int lastGeneratedNum;
+    private Dictionary<string,int> m_IDs = new Dictionary<string, int>();
 
     //Example of RunTime ID SCL005
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int CalculateMaxNumber(int lenght)
     {
-        
-    }
-
-    public string GenerateID(string prefix, int lenght, int maxNum)
-    {
-
-        //Create the variable
-        int number = 1;
-
-        //adding the houses
-        for (int i = 0; i < maxNum; i++)
+        /*
+        //int number = 9;
+        for (int i = 0; i < lenght; i++)
         {
             //increase the houses based on the number of indexes that we need
             number = number * 10;
+            number = number + 9;
+            
+        }
+        */
+        int max = (int)Mathf.Pow(10, lenght) - 1;
+        return max;
+    }
+
+    public string GenerateID(string prefix, int lenght)
+    {
+        int number = 0;
+        //if it has the prefix extract the number for it
+        if (m_IDs.ContainsKey(prefix))
+        {
+            number = m_IDs[prefix];
+
+            if (number >= CalculateMaxNumber(lenght))
+                number = 0;
+            else
+                number++; 
         }
 
-        string numberString = number.ToString();
-
-        return prefix + number;
+        m_IDs[prefix] = number;
+        string finalID = prefix + number.ToString().PadLeft(lenght, '0');
+        return finalID;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    /*
+    public void RemoveIDWhere(string ID)
     {
-        
+        string prefix = Regex.Replace(ID, @"\d", "");
+        int number = int.Parse(Regex.Replace(ID, "[^0-9]", ""));
+        m_IDs.Remove(prefix,out number);
     }
+    */  
+
 }
