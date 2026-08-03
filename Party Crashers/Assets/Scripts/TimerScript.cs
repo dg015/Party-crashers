8d0fb91dbfaca1d71333ec2958955895ceee79d1
dg015 <bruno.barbosa8@yahoo.com>
2026-08-02 23:07:39 -0300

    Timer tool created

---
+++ Party Crashers/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class TimerScript
+{
+    private float m_elapsedTime;
+    private bool m_isPaused;
+    private bool m_isComplete;
+    private bool m_isAutoReset;
+    private bool m_autoPause;
+
+    public bool Tick(float duration, float deltaTime)
+    {
+        if (m_isPaused)
+            return false;
+
+        //if it has autopause and it has already been complete then stop it
+        if (m_isComplete && m_autoPause)
+            return false;
+
+        m_elapsedTime += deltaTime;
+        if (m_elapsedTime >= duration)
+        {
+            m_isComplete = true;
+
+            //if its auto reset then reset timer
+            if(m_isAutoReset)
+                m_elapsedTime = 0;
+
+            return true;
+        }
+        else
+        {
+            m_isComplete = false;
+            return false;
+        }
+    }
+
+    public float GetElapsedTime()
+    {
+        return m_elapsedTime;
+    }
+
+    //Set the timer to auto reset (back to 0) when finished)
+    public void SetRepeating(bool status)
+    {
+        m_isAutoReset = status;
+    }
+
+    //Sets auto pause so its stops after its complete for the first time
+    //(good if u want triggers)
+    public void SetAutoPause(bool status)
+    {
+        m_autoPause = status;
+    }
+
+    //Returns if its complete or not
+    public bool IsComplete()
+    {
+        return m_isComplete;
+    }
+
+    //Toggles pause
+    public void PauseTimer(bool status)
+    {
+         m_isPaused = status;
+    }
+
+    //Resets timer
+    public void ResetElapsedTime()
+    {
+        m_elapsedTime = 0;
+        m_isComplete = false;
+    }
+}
